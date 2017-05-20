ca708b2448f2aa0c78e2d4214e7d159e9975a2d0
Nicola De Fiorenze <nicola.defiorenze@ennova-research.com>
2017-05-20 12:52:43 +0200

    add .gitignore file

---
+++ app/src/main/java/com/nicoladefiorenze/room/database/User.kt
@@ -0,0 +1,10 @@
+package com.nicoladefiorenze.room.database
+
+/**
+ * Project: Room<br/>
+ * <p> This doc comment should contain a short class description.<p/>
+ * <br/>
+ * created on: 2017-05-20
+ *
+ * @author Nicola De Fiorenze
+ */
